c47d5ea29c452947785c1e93148357c7dc74c429
nedzipatrycja <nedzipatrycja@users.noreply.github.com>
2020-11-08 13:24:15 +0100

    zad 0 10 i 5

---
+++ prog0.py
@@ -0,0 +1,9 @@
+print('Hello word')
+imie='Wikroria'
+Nazwisko='Nazwisko'
+ilosc_ksiazek = 10
+srednia = 4.5
+
+print('hello' + imie +'  '+ Nazwisko)
+print(' srednia: str(srednia))')
+print('ilosc:'+ str(ilosc_ksiazek))
---
+++ prog5.py
@@ -0,0 +1,7 @@
+marka = 'Peguot'
+ilosc_drzwi = 5
+pojemnosc = 1.3
+
+
+marka_up = marka.upper()
+print(marka_up)
